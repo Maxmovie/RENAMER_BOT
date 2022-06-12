69ff487954484dcabe960cff1b8b4d2b26e7ed6d
Maxmovie <106411226+Maxmovie@users.noreply.github.com>
2022-06-12 07:21:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,14 +8,14 @@ async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text(text =f"""
 	Hello {message.from_user.first_name }
-	__I am file renamer bot, Please sent any telegram 
+	__I am file renamer bot,Made by Abel bro @Maxmoviesorginal - Please sent any telegram 
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup([[
-          InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Mo_Tech_Group"), 
-	  InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/MoTech_YT")
+          InlineKeyboardButton("Support 🌼" ,url="https://t.me/maxmoviesorginal"), 
+	  InlineKeyboardButton("Join as 🤗", url="https://t.me/maxmoviesorginal")
           ],[
-          InlineKeyboardButton("🧩 Deploy 🧩", url="https://youtu.be/7ALYUk-rYMc")
+          InlineKeyboardButton("✨ My deploy link ✨", url="")
           ]]
           )
         )
